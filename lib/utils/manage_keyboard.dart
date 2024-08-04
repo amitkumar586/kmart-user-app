908711379ee176f048beb5212750ae05f90c7c3e
Amit Kumar gangwar <amitkumargangwar189@gmail.com>
2024-08-04 07:32:52 +0530

    utils

---
+++ lib/utils/manage_keyboard.dart
@@ -0,0 +1,10 @@
+import '../const/consts.dart';
+
+class KeyboardUtil {
+  static void hideKeyboard(BuildContext context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
+}
