52f9c36dfb8731880dd5e21865b96bfbe73b116b
Amit Kumar gangwar <amitkumargangwar189@gmail.com>
2024-08-04 09:48:04 +0530

    scope

--- lib/services/get_server_key.dart
+++ lib/services/get_server_key.dart
@@ -1,5 +1,10 @@
 class GetServerKey {
   Future<String> getServerKeyToken() async {
+    final scope = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/firebase.database',
+      'https://www.googleapis.com/auth/firebase.messaging',
+    ];
     return "";
   }
 }
